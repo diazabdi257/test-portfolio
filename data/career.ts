bfc6fd821e9180460ee17125e2c468926e9008ba
diazabdi257 <diazabdi2003@gmail.com>
2026-04-17 01:24:45 +0700

    karir fixed

--- data/career.ts
+++ data/career.ts
@@ -11,13 +11,13 @@ export const careerJourney: CareerItem[] = [
     company: "PT. PYX SOLUSI TEKNOLOGI",
     period: "November 2025 - Desember 2025",
     summary:
-      "Melakukan rebuild website perusahaan menggunakan ReactJS, Next.js, dan Tailwind CSS serta integrasi API untuk meningkatkan performa dan pengalaman pengguna.",
+      "Bertanggung jawab atas akurasi input dan pembaruan lebih dari 70 dokumen fisik ke dalam basis data perusahaan. Mengaudit integritas data untuk mengidentifikasi dan mengoreksi inkonsistensi sebelum penyerahan akhir, serta menyusun laporan harian berbasis Microsoft Excel.",
   },
   {
     role: "Web Development Intern",
     company: "PT. Falcon Pictures",
     period: "July 2024 - August 2024",
     summary:
-      "Melakukan rebuild website perusahaan menggunakan ReactJS, Next.js, dan Tailwind CSS serta integrasi API untuk meningkatkan performa dan pengalaman pengguna.",
+      "Membangun ulang arsitektur front-end platform Edufy menggunakan Next.js, React, dan Tailwind CSS. Ruang lingkup pekerjaan mencakup pengembangan komponen UI yang interaktif, serta integrasi REST API untuk menggerakkan fitur inti platform seperti manajemen akun pengguna dan data kursus.",
   },
 ];
